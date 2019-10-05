b79d6ba0aed3d3742328cfdd2d41adb5cefabcdc
mcarmenzv <mcarmenzv@gmail.com>
2019-10-05 17:04:00 +0200

    Agregadas vistas y plantilla para el detalle del post del blog así como también CSS para el sitio.

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 from .models import Post
 from django.contrib.auth.models import User
+
 
 def post_list(request):
 	me = User.objects.get(username='mcarmenzv')
 	posts =  Post.objects.filter(author=me)
 	return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
